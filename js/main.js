f98a5619d7f7ad034b5ce17f23975ef53452f02e
hazelmunez721 <hazelmaegonzalesmunez@gmail.com>
2023-06-14 07:42:04 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,155 @@
+/* =========== Show Navbar =========== */
+const navbar = document.querySelector(".nav-list");
+const hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", () => {
+    navbar.classList.toggle("show");
+});
+
+window.addEventListener("scroll", () => {
+    navbar.classList.remove("show");
+});
+
+/* =========== User Data =========== */
+
+const counters = [...document.querySelectorAll(".user-data .number")];
+const speed = 5000; // change animation speed here
+
+const start = () => {
+    counters.forEach((counter) => {
+        const updateCount = () => {
+            const target = +counter.getAttribute("data-target");
+            const count = +counter.innerText;
+            const inc = target / speed;
+            if (count < target) {
+                counter.innerText = Math.ceil(count + inc);
+                setTimeout(updateCount, 1);
+            } else {
+                counter.innerText = target + "+";
+            }
+        };
+        updateCount();
+    });
+};
+
+window.addEventListener(
+    "scroll",
+    () => {
+        if (window.pageYOffset >= 250) {
+            start();
+        }
+    }, { passive: true }
+);
+
+/* ========== Swiper =========== */
+const swiper = new Swiper(".mySwiper", {
+    grabCursor: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".custom-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".custom-button-prev",
+        prevEl: ".custom-button-next",
+    },
+    breakpoints: {
+        567: {
+            slidesPerView: 2,
+        },
+        996: {
+            slidesPerView: 3,
+        },
+    },
+});
+
+/* =========== Scroll Top =========== */
+const scrollTop = document.querySelector(".scroll-top");
+
+scrollTop.addEventListener("click", () => {
+    window.scrollTo({
+        left: 0,
+        top: 0,
+        behavior: "smooth",
+    });
+});
+
+window.addEventListener("scroll", (e) => {
+    const scrollHeight = window.pageYOffset;
+
+    if (scrollHeight > 300) {
+        scrollTop.classList.add("show");
+    } else {
+        scrollTop.classList.remove("show");
+    }
+});
+
+/* ========== Scroll Reveal ============ */
+const scroll = ScrollReveal({
+    distance: "60px",
+    duration: 2500,
+    delay: 300,
+    reset: true,
+});
+
+scroll.reveal(`.hero .col:last-child`, { delay: 600 });
+
+scroll.reveal(
+    `.about1 .col:last-child h3,.about1 .col:last-child p,
+  .about-1 .col:last-child li`, {
+        origin: "bottom",
+        interval: 100,
+    }
+);
+
+// scroll.reveal(
+//     `.about-2 .col:last-child h3,.about-2 .col:last-child p,
+// .about-2 .col:last-child li`, {
+//         origin: "bottom",
+//         interval: 100,
+//     }
+// );
+
+// scroll.reveal(
+//     `.about-3 .col:last-child h3,.about-3 .col:last-child p,
+// .about-3 .col:last-child li`, {
+//         origin: "bottom",
+//         interval: 100,
+//     }
+// );
+
+scroll.reveal(
+    `.about-4 .col:last-child h3,.about-4 .col:last-child p,
+  .about-4 .col:last-child li, .hero .col h1, .hero .col p,.hero .col .btn`, {
+        origin: "top",
+        interval: 100,
+    }
+);
+
+scroll.reveal(
+    `.about1 .col:first-child img, .languages img, .testimonials .row`, {
+        origin: "top",
+    }
+);
+
+scroll.reveal(`.about1 .col:last-child img`, { origin: "right" });
+scroll.reveal(`.about-2 .col:first-child img`, { origin: "left" });
+scroll.reveal(`.about-2 .col:last-child img`, { origin: "right" });
+scroll.reveal(`.about-3 .col:first-child img`, { origin: "right" });
+scroll.reveal(`.about-4 .col:first-child img`, { origin: "right" });
+scroll.reveal(`.about-5 .col:first-child img`, { origin: "right" });
+scroll.reveal(`.about-5 .col:last-child img`, { origin: "top" });
+scroll.reveal(`.project img`, { origin: "top" });
+
+/*SCROLL WORK*/
+sr.reveal('.work__img', { interval: 200 });
+
+scroll.reveal(`.footer .col div,.footer .col p`, {
+    origin: "top",
+    interval: 100,
+});
+
+const projectLightbox = GLightbox({
+    selector: '.project-lightbox'
+});
